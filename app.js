9ab3b494dabb7eda01e3df4e36ce8a6d6146dd33
ArinaBogan <arina.bogan@gmail.com>
2023-09-16 18:08:21 +0300

    added OOP

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const dynamicTime = document.querySelector('.time');
-const dynamicYear = document.querySelector('.date');
 
-function doDate(dynamicTime, dynamicYear) {
-    const d = new Date();
-    dynamicTime.innerHTML = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-    dynamicYear.innerHTML = 'since.' + d.getFullYear();
+class Watch {
+    constructor() {
+        setInterval(this.doDate, 10, this.dynamicYear)
+        setInterval(this.doTime, 10, this.dynamicTime)
+    }
+
+    dynamicYear = document.querySelector('.date');
+    doDate(dynamicYear) {
+        const d = new Date();
+        dynamicYear.innerHTML = 'since.' + d.getFullYear();
+    }
+
+    dynamicTime = document.querySelector('.time');
+    doTime(dynamicTime) {
+        const t=new Date();
+        dynamicTime.innerHTML = t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds();
+    }
+
 }
-setInterval(doDate, 100, dynamicTime, dynamicYear)
+const watch = new Watch();
